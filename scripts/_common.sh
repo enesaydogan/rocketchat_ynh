46609ad08d2a04bfa4fdbb220efe6eb144da761f
enesaydogan <enesaydogan@hotmail.com>
2018-06-22 15:23:03 +0300

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -40,18 +40,6 @@ installnode(){
 
 installdeps(){
 
-  if [ $(dpkg --print-architecture) == "armhf" ]; then
-    #Install mongodb for debian armhf
-    sudo apt-get update
-    sudo apt-get install -y mongodb-server
-  else
-    #Install mongodb for debian x86/x64
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-    echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-    sudo apt-get update
-    sudo apt-get install -y mongodb-org
-  fi
-
   # start mongodb service
   sudo systemctl enable mongod.service
   sudo systemctl start mongod.service
